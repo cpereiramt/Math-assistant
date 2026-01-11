0e527f69cde003e3c3eeed486d1d94b274a80e17
Clayton Pereira <cpereiramt@gmail.com>
2026-01-11 14:05:35 -0400

    fix: adjusting google oauth credentials

--- src/main/java/com/claySoftware/MathExpAssistant/config/CustomOAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/claySoftware/MathExpAssistant/config/CustomOAuth2AuthenticationSuccessHandler.java
@@ -12,26 +12,28 @@ import java.io.IOException;
 
 @Component
 public class CustomOAuth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
-    private String DEFAULT_REDIRECT_URL;
+
+    private String DEFAULT_REDIRECT_URL = "/api/docs"; // redirect for web
 
     private final JwtTokenProvider jwtTokenProvider;
-    // private static final String DEFAULT_REDIRECT_URL = "mathassistant://"; redirect for mobile
+    // private static final String DEFAULT_REDIRECT_URL = "mathassistant://";
+    // redirect for mobile
 
     public CustomOAuth2AuthenticationSuccessHandler(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, IOException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, IOException {
         // Create the token for authentication
         String token = jwtTokenProvider.createToken(authentication);
 
         // build the redirectURL
         String redirectUrl = DEFAULT_REDIRECT_URL + "?token=" + token;
-        System.out.print("token ------> "+ token);
+        System.out.print("token ------> " + token);
 
         // Redirect to the mobile app or to web app
-        response.sendRedirect(redirectUrl);
+        response.sendRedirect(DEFAULT_REDIRECT_URL);
     }
 }
